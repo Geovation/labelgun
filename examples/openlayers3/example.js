a9445acadb31d35fbf68c5e04e4d5c2c072ab0f9
James Milner <jamesmilner22@hotmail.com>
2017-01-07 20:21:11 +0000

    Fix Firefox error on SVG rendering for icons

--- examples/openlayers3/example.js
+++ examples/openlayers3/example.js
@@ -26,13 +26,13 @@ SystemJS.import('labelgun').then(function(labelgun) {
         target: 'map',
         view: new ol.View({
             center: [0, 0],
-            zoom: 2
+            zoom: 4
         })
     });
 
 
-    var hideLabel = function(label){ console.log(label.labelObject); label.labelObject.getImage().setOpacity(0);} 
-    var showLabel = function(label){ label.labelObject.getImage().setOpacity(1);}
+    var hideLabel = function(label){ console.log("hide"); label.labelObject.getImage().setOpacity(0);} 
+    var showLabel = function(label){ console.log("show");  label.labelObject.getImage().setOpacity(1);}
     labelEngine = new labelgun.default(hideLabel, showLabel);
 
     var ghostZoom = map.getView().getZoom();
@@ -90,19 +90,20 @@ SystemJS.import('labelgun').then(function(labelgun) {
         labelWidth = labelWidth + 10;
 
         var iconSVG = '<svg ' +
-                    'version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" ' +
+                    'version="1.1" xmlns="http://www.w3.org/2000/svg" ' +
                     'x="0px" y="0px" width="' + labelWidth + 'px" height="16px" ' +
-                    'viewBox="0 0 ' + labelWidth + ' 16" enable-background="new 0 0 ' + labelWidth + ' 16" xml:space="preserve">'+
+                    'viewBox="0 0 ' + labelWidth + ' 16" enable-background="new 0 0 ' + labelWidth + ' 16" >'+
+                        '<g>' +
                         '<rect x="0" y="0" width="' + labelWidth + '" height="16" stroke="#000000" fill="#DEEFAE" stroke-width="2"></rect>' +
                         '<text x="5" y="13" fill="#000000" font-family="Arial" font-size="12" font-weight="normal">' + text + '</text>' +
-                '</svg>';
-
-        var imageElement = new Image();
-        imageElement.src = 'data:image/svg+xml,' + escape( iconSVG );
+                        '</g>' +
+                    '</svg>';
+                    
+       var src = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent( iconSVG );
 
         var iconStyle = new ol.style.Style({
             "image": new ol.style.Icon({
-                "img": imageElement,
+                src : src,
                 "imgSize":[labelWidth, 66],
                 "anchor": [0.5, 0.5],
                 "offset": [0, -50]
@@ -118,14 +119,14 @@ SystemJS.import('labelgun').then(function(labelgun) {
 
     function getBoundingBox(center, labelWidth) {
 
-      //  console.log(label);
-        // var middle = map.getCoordinateFromPixel(center);
-        var half = labelWidth / 2;
-        var fontSize = 12;
-        center = map.getPixelFromCoordinate(center);
+        var halfLabelWidth = labelWidth / 2;
+        var halfFontSize = 12 / 2;
+        var pixelCenter = map.getPixelFromCoordinate(center);
 
-        var bottomLeft =  map.getCoordinateFromPixel([center[0] - half, center[1] - fontSize]);
-        var topRight =  map.getCoordinateFromPixel([center[0] + half, center[1] + fontSize]);
+        var bottomLeft =  map.getCoordinateFromPixel([pixelCenter[0] - halfLabelWidth, pixelCenter[1] - halfFontSize]);
+        var topRight =  map.getCoordinateFromPixel([pixelCenter[0] + halfLabelWidth, pixelCenter[1] + halfFontSize]);
+
+        console.log(bottomLeft, topRight);
 
         return boundingBox =  {
             bottomLeft :  bottomLeft,
